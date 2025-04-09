0130f4434a2de5d2f0826fee114f732fa8c0cce9
Mike <spormuv@gmail.com>
2025-04-10 01:39:04 +0300

    feat: implement login screen

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,17 +1,53 @@
+import { useState } from 'react';
 import { Link } from 'react-router';
+import PasswordInput from '../../components/Input/PasswordInput';
 import Navbar from '../../components/Navbar/Navbar';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError('Please enter a valid email address.');
+      return;
+    }
+
+    if (!password) {
+      setError('Please enter a password.');
+      return;
+    }
+
+    setError('');
+    // Login API Call
+  }
+
   return (
     <>
       <Navbar />
 
       <div className='flex items-center justify-center mt-28'>
         <div className='w-96 border rounded bg-white px-7 py-10'>
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className='text-2xl mb-7'>Login</h4>
 
-            <input type='text' placeholder='Email' className='input-box' />
+            <input
+              type='text'
+              placeholder='Email'
+              className='input-box'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
 
             <button type='submit' className='btn-primary'>
               Login
